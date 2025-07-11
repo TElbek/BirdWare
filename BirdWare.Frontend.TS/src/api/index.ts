0ebc0b512c6e0460c7d74f8e6a0c42b016e73bcb
Thomas Elbek <vadholm@hotmail.com>
2025-07-11 17:32:35 +0200

    authentication added to api/index.ts

--- BirdWare.Frontend.TS/src/api/index.ts
+++ BirdWare.Frontend.TS/src/api/index.ts
@@ -1,7 +1,17 @@
 import axios from 'axios';
+import { useAuthenticateStore } from '@/stores/authenticate.js';
 
 const api = axios.create({
   baseURL: import.meta.env.VITE_VUE_API_BASE_URL
+});
+
+api.interceptors.request.use((config) => {
+  const authenticate = useAuthenticateStore();
+
+  if (authenticate.isLoggedIn) {
+    config.headers.Authorization = "Bearer " + authenticate.jwtToken;
+  }
+  return config;
 });
 
 export default api;
