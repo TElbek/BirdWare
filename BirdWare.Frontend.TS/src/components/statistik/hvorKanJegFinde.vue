7e99f169683079dfe5192544b9419cc2b82bc534
Thomas Elbek <vadholm@hotmail.com>
2025-10-28 08:39:26 +0100

    style: adjust margin for title in forskel and hvorKanJegFinde components

--- BirdWare.Frontend.TS/src/components/statistik/hvorKanJegFinde.vue
+++ BirdWare.Frontend.TS/src/components/statistik/hvorKanJegFinde.vue
@@ -1,19 +1,22 @@
 <template>
-    <div class="pt-2">
-        <tw-text-sizeable>{{route.meta.title}}</tw-text-sizeable>        
-        <tw-grid-cols-five :count="byLokalitet.size" class="mt-2">
-            <div v-for="[key, value] in byLokalitet">
-                <tw-card>
-                    <tw-card-header :caption="`${key} ${Math.round(value[0].distance)} km`" :count="value.length" :show-count="true"></tw-card-header>
-                    <tw-flex>
-                        <template v-for="item in arterSorteret(value)">
-                            <art-navn :art-navn="item.artNavn" :art-id="item.artId" :speciel="false"
-                                :su="false"></art-navn>
-                        </template>
-                    </tw-flex>
-                </tw-card>
-            </div>
-        </tw-grid-cols-five>
+    <div class="mt-3">
+        <tw-text-sizeable>{{ route.meta.title }}</tw-text-sizeable>
+        <div class="max-h-160 xl:max-h-180 overflow-auto">
+            <tw-grid-cols-five :count="byLokalitet.size" class="mt-2 mb-3">
+                <div v-for="[key, value] in byLokalitet">
+                    <tw-card>
+                        <tw-card-header :caption="`${key} ${Math.round(value[0].distance)} km`" :count="value.length"
+                            :show-count="true"></tw-card-header>
+                        <tw-flex>
+                            <template v-for="item in arterSorteret(value)">
+                                <art-navn :art-navn="item.artNavn" :art-id="item.artId" :speciel="false"
+                                    :su="false"></art-navn>
+                            </template>
+                        </tw-flex>
+                    </tw-card>
+                </div>
+            </tw-grid-cols-five>
+        </div>
     </div>
 </template>
 
@@ -22,7 +25,7 @@ import api from '@/api';
 import { reactive, computed, onMounted } from 'vue';
 import { type hvorKanJegFindeType } from '@/types/hvorKanJegFindeType';
 import ArtNavn from '../main/artNavn.vue';
-import {useRoute} from 'vue-router';
+import { useRoute } from 'vue-router';
 
 const route = useRoute();
 
