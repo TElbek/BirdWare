0532f113ac3b5ae3e66da844c40adee5cbc145d4
Thomas Elbek <vadholm@hotmail.com>
2025-12-15 08:02:30 +0100

    Tilføjet observation-map-logic composable til at håndtere leaflet logik

--- BirdWare.Frontend.TS/src/components/observation/leaflet/observationMap.vue
+++ BirdWare.Frontend.TS/src/components/observation/leaflet/observationMap.vue
@@ -5,37 +5,21 @@
 <script setup lang="ts">
 import api from '@/api';
 import { ref, onMounted, watch, computed } from 'vue';
-import type { Feature } from 'geojson';
-import * as L from 'leaflet';
 import { useObsSelectionStore } from '@/stores/obs-selection-store';
 import type { birdwareGeoJson } from '@/types/birdwareGeoJsonType';
-
-const obsSelectionStore = useObsSelectionStore();
+import { useObservationMapLogic } from '@/composables/observation-map-logic';
 
 const geoJsonObj = ref({} as birdwareGeoJson)
-
-const map = ref();
-const observationLayer = ref({} as L.Layer);
-const hasLayer = ref(false);
+const obsSelectionStore = useObsSelectionStore();
+const observationMapLogic = useObservationMapLogic(emitAddTag);
 
 const emit = defineEmits(['addtag']);
 const queryString = computed(() => { return JSON.stringify(obsSelectionStore.selectedTags) });
 
 const hasData = ref(false);
 
-const svgTemplateBlue = `<svg xmlns="http://www.w3.org/2000/svg">
-   <circle cx="10" cy="10" r="8" fill="#0000ff" />
-   Sorry, your browser does not support inline SVG.
-</svg>`;
-
-const svgTemplateRed = `<svg xmlns="http://www.w3.org/2000/svg">
-   <circle cx="10" cy="10" r="8" fill="#ff0000" />
-   Sorry, your browser does not support inline SVG.
-</svg>`;
-
-
 onMounted(() => {
-    initializeLeaflet();
+    observationMapLogic.initializeLeaflet();
     getGeoJSon();
 });
 
@@ -47,82 +31,13 @@ function getGeoJSon() {
     });
 }
 
-function initializeLeaflet() {
-    map.value = L.map('map');
-    L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-        maxZoom: 15,
-        attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-    }).addTo(map.value);
-}
-
-function addPointsToMap() {
-    resetGeoJson();
-    observationLayer.value = L.geoJSON(geoJsonObj.value as any, {
-        pointToLayer: function (feature: Feature, latlng: L.LatLng) {
-            let marker = L.marker(latlng, {
-                icon: createMapIcon({ width: 22, countIsAboveAverage: feature.properties?.countIsAboveAverage })
-            });
-            marker.on('mouseover', function (ev) {
-                marker.openPopup();
-            });
-            marker.on('click', function (ev) {
-                whenFeatureClicked(ev);
-            });
-            return marker;
-        },
-        onEachFeature: function (feature, layer) {
-            layer.bindPopup('<strong>' + feature.properties?.name + '</strong><br/> ' + feature.properties?.count + ' observationer')
-        }
-    }).addTo(map.value);
-
-    fitBounds();
-    toggleHasLayer();
-}
-
-function createMapIcon({ countIsAboveAverage = false, width = 22, color = '#0000ff' } = {}) {
-    const height = Math.round(width * (300 / 240)); // ratio 1.25
-    const html = L.Util.template(countIsAboveAverage ? svgTemplateRed : svgTemplateBlue, {
-        width,
-        height,
-        color,
-    });
-    return L.divIcon({
-        html,
-        iconSize: [width, height]
-    });
-}
-
-function whenFeatureClicked(e: L.LeafletMouseEvent) {
-    if (obsSelectionStore.hasTagWithName(e.target.feature.properties.name)) {
-        emit('addtag', e.target.feature.properties.name);
+function emitAddTag(name: string) {
+    if (obsSelectionStore.hasTagWithName(name)) {
+        emit('addtag', name);
     }
     else {
-        emit('addtag', e.target.feature.properties.name);
+        emit('addtag', name);
         obsSelectionStore.SetGroupingId(0);
-    }
-}
-
-function resetGeoJson() {
-    if (hasLayer.value) {
-        removeLayer();
-    }
-}
-
-function removeLayer() {
-    observationLayer.value.remove();
-    toggleHasLayer();
-}
-
-function toggleHasLayer() {
-    hasLayer.value = !hasLayer.value;
-}
-
-function fitBounds() {
-    if (observationLayer) {
-        let layerList: L.Layer[] = [observationLayer.value as L.Layer];
-        let featureGroup = new L.FeatureGroup<L.Layer>(layerList);
-        let bounds = featureGroup.getBounds();
-        map.value.fitBounds(bounds, { padding: [20, 20] });
     }
 }
 
@@ -131,6 +46,6 @@ watch(() => obsSelectionStore.selectedTags, (newValue) => {
 });
 
 watch(() => geoJsonObj.value, (newValue) => {
-    if (hasData.value) addPointsToMap();
+    if (hasData.value) observationMapLogic.addPointsToMap(geoJsonObj.value);
 });
 </script>
---
+++ BirdWare.Frontend.TS/src/composables/observation-map-logic.ts
@@ -0,0 +1,96 @@
+import type { Feature } from 'geojson';
+import type { birdwareGeoJson } from '@/types/birdwareGeoJsonType';
+import * as L from 'leaflet';
+import { ref } from 'vue';
+
+export function useObservationMapLogic(emitTagCallback: any) {
+    const map = ref();
+    const observationLayer = ref({} as L.Layer);
+    const hasLayer = ref(false);
+
+    const svgTemplateBlue = `<svg xmlns="http://www.w3.org/2000/svg">
+                <circle cx="10" cy="10" r="8" fill="#0000ff" />
+            </svg>`;
+
+    const svgTemplateRed = `<svg xmlns="http://www.w3.org/2000/svg">
+                <circle cx="10" cy="10" r="8" fill="#ff0000" />
+            </svg>`;
+
+    function initializeLeaflet() {
+        map.value = L.map('map');
+        L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+            maxZoom: 15,
+            attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+        }).addTo(map.value);
+    }
+
+    function addPointsToMap(geoJson: birdwareGeoJson) {
+        resetGeoJson();
+        observationLayer.value = L.geoJSON(geoJson as any, {
+            pointToLayer: function (feature: Feature, latlng: L.LatLng) {
+                let marker = L.marker(latlng, {
+                    icon: createMapIcon({ width: 22, countIsAboveAverage: feature.properties?.countIsAboveAverage })
+                });
+                marker.on('mouseover', function (ev) {
+                    marker.openPopup();
+                });
+                marker.on('click', function (ev) {
+                    whenFeatureClicked(ev);
+                });
+                return marker;
+            },
+            onEachFeature: function (feature, layer) {
+                layer.bindPopup('<strong>' + feature.properties?.name + '</strong><br/> ' + feature.properties?.count + ' observationer')
+            }
+        }).addTo(map.value);
+
+        fitBounds();
+        toggleHasLayer();
+    }
+
+    function createMapIcon({ countIsAboveAverage = false, width = 22, color = '#0000ff' } = {}) {
+        const height = Math.round(width * (300 / 240)); // ratio 1.25
+        const html = L.Util.template(countIsAboveAverage ? svgTemplateRed : svgTemplateBlue, {
+            width,
+            height,
+            color,
+        });
+        return L.divIcon({
+            html,
+            iconSize: [width, height]
+        });
+    }
+
+    function whenFeatureClicked(e: L.LeafletMouseEvent) {
+        emitTagCallback(e.target.feature.properties.name)
+    }
+
+    function resetGeoJson() {
+        if (hasLayer.value) {
+            removeLayer();
+        }
+    }
+
+    function removeLayer() {
+        observationLayer.value.remove();
+        toggleHasLayer();
+    }
+
+    function toggleHasLayer() {
+        hasLayer.value = !hasLayer.value;
+    }
+
+    function fitBounds() {
+        if (observationLayer) {
+            let layerList: L.Layer[] = [observationLayer.value as L.Layer];
+            let featureGroup = new L.FeatureGroup<L.Layer>(layerList);
+            let bounds = featureGroup.getBounds();
+            map.value.fitBounds(bounds, { padding: [20, 20] });
+        }
+    }
+
+    return {
+        initializeLeaflet,
+        addPointsToMap
+    }
+}
