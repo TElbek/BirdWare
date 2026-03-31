69356ee75d1b61a7f14f189f6681e54a8015699f
Thomas Elbek <vadholm@hotmail.com>
2026-03-31 11:10:33 +0200

    Refactor ObservationsByTagsQuery to streamline filtering logic and improve readability

--- BirdWare.EF/Queries/ObservationsByTagsQuery.cs
+++ BirdWare.EF/Queries/ObservationsByTagsQuery.cs
@@ -16,11 +16,7 @@ namespace BirdWare.EF.Queries
             foreach (var tagType in TagsGroupedByTagType(tagList))
             {
                 var observationTagFilter = GetFilterForTagType<IObservationTagFilter>(tagType.Key);
-                var result = observationTagFilter.Filter(tagType.Value, observations);
-                if (result is IQueryable<Observation> observationResult)
-                {
-                    observations = observationResult;
-                }
+                observations = observationTagFilter.Filter(tagType.Value, observations);
             }
 
             return GenerateResultSet(observations);
@@ -28,18 +24,16 @@ namespace BirdWare.EF.Queries
 
         private static List<VObs> GenerateResultSet(IQueryable<Observation> observations)
         {
-            var listeAfObservationer = observations
-                    .Include(i => i.Fugletur)
-                    .Include(i => i.Fugletur.Lokalitet)
-                    .Include(i => i.Fugletur.Lokalitet.Region)
-                    .Include(i => i.Art)
-                    .Include(i => i.Art.Gruppe)
-                    .Include(i => i.Art.Gruppe.Familie)
-                    .OrderByDescending(r => r.FugleturId)
-                    .Take(200)
-                    .ToList();
-
-            return [.. listeAfObservationer.Select(s => VObs.MapFromObservation(s))];
+            return [.. observations
+                        .Include(i => i.Fugletur)
+                        .Include(i => i.Fugletur.Lokalitet)
+                        .Include(i => i.Fugletur.Lokalitet.Region)
+                        .Include(i => i.Art)
+                        .Include(i => i.Art.Gruppe)
+                        .Include(i => i.Art.Gruppe.Familie)
+                        .OrderByDescending(r => r.FugleturId)
+                        .Take(200)
+                        .Select(s => VObs.MapFromObservation(s))];
         }
     }
 }
