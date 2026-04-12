ceef192db26d476a4ef8af2d35058cb202813f86
Thomas Elbek <vadholm@hotmail.com>
2026-04-12 09:50:29 +0200

    Refactor AnkomtsDage queries into separate methods.

--- BirdWare.EF/Queries/AnkomtsDagQuery.cs
+++ BirdWare.EF/Queries/AnkomtsDagQuery.cs
@@ -9,35 +9,9 @@ namespace BirdWare.EF.Queries
         {
             var ankomtsDage = new List<AnkomstDag>();
 
-            var iAarQuery = (from obs in birdWareContext.Observation
-                         join art in birdWareContext.Art on obs.ArtId equals art.Id
-                         join grupper in birdWareContext.Gruppe on art.GruppeId equals grupper.Id
-                         join fugletur in birdWareContext.Fugletur on obs.FugleturId equals fugletur.Id
-                         where grupper.FamilieId == familieId && 
-                               art.SU == false && 
-                               fugletur.Dato.HasValue && fugletur.Dato.Value.Year == DateTime.Now.Year
-                         group obs by new { art.Id, art.Navn } into g
-                         select new
-                         {
-                             ArtId = g.Key.Id,
-                             ArtNavn = g.Key.Navn,
-                             SetIaarDato = g.Min(o => o.Fugletur.Dato)
-                         }).ToList();
+            var iAarQuery = GetIAarQuery(familieId);
 
-            var ankomstDagQuery = (from obs in birdWareContext.Observation
-                         join art in birdWareContext.Art on obs.ArtId equals art.Id
-                         join grupper in birdWareContext.Gruppe on art.GruppeId equals grupper.Id
-                         join fugletur in birdWareContext.Fugletur on obs.FugleturId equals fugletur.Id
-                         where grupper.FamilieId == familieId && art.SU == false
-                         group obs by new { art.Id, art.Navn, Dato = fugletur.Dato ?? DateTime.MinValue } into g
-                         let dayOfYear = g.Min(o => g.Key.Dato.DayOfYear)
-                         group new { g.Key.Id, g.Key.Navn, dayOfYear } by new {g.Key.Id, g.Key.Navn } into finalGroup
-                         select new
-                         {
-                             ArtId = finalGroup.Key.Id,
-                             ArtNavn = finalGroup.Key.Navn,
-                             AnkomstDato = new DateTime(2026, 1, 1).AddDays((int)finalGroup.Average(x => x.dayOfYear) - 1)
-                         }).ToList();
+            var ankomstDagQuery = GetAnkomstDagQuery(familieId);
 
             foreach (var item in ankomstDagQuery)
             {
@@ -52,5 +26,48 @@ namespace BirdWare.EF.Queries
 
             return ankomtsDage;
         }
+
+        private List<AnkomstDagBeregning> GetAnkomstDagQuery(long familieId)
+        {
+            return [.. (from obs in birdWareContext.Observation
+                    join art in birdWareContext.Art on obs.ArtId equals art.Id
+                    join grupper in birdWareContext.Gruppe on art.GruppeId equals grupper.Id
+                    join fugletur in birdWareContext.Fugletur on obs.FugleturId equals fugletur.Id
+                    where grupper.FamilieId == familieId && art.SU == false
+                    group obs by new { art.Id, art.Navn, Dato = fugletur.Dato ?? DateTime.MinValue } into g
+                    let dayOfYear = g.Min(o => g.Key.Dato.DayOfYear)
+                    group new { g.Key.Id, g.Key.Navn, dayOfYear } by new { g.Key.Id, g.Key.Navn } into finalGroup
+                    select new AnkomstDagBeregning
+                    {
+                        ArtId = finalGroup.Key.Id,
+                        ArtNavn = finalGroup.Key.Navn,
+                        AnkomstDato = new DateTime(2026, 1, 1).AddDays((int)finalGroup.Average(x => x.dayOfYear) - 1)
+                    })];
+        }
+
+        private List<AnkomstDagBeregning> GetIAarQuery(long familieId)
+        {
+            return [.. (from obs in birdWareContext.Observation
+                    join art in birdWareContext.Art on obs.ArtId equals art.Id
+                    join grupper in birdWareContext.Gruppe on art.GruppeId equals grupper.Id
+                    join fugletur in birdWareContext.Fugletur on obs.FugleturId equals fugletur.Id
+                    where grupper.FamilieId == familieId &&
+                          art.SU == false &&
+                          fugletur.Dato.HasValue && fugletur.Dato.Value.Year == DateTime.Now.Year
+                    group obs by new { art.Id, art.Navn } into g
+                    select new AnkomstDagBeregning
+                    {
+                        ArtId = g.Key.Id,
+                        ArtNavn = g.Key.Navn,
+                        SetIaarDato = g.Min(o => o.Fugletur.Dato)
+                    })];
+        }
     }
-}
+
+    internal class AnkomstDagBeregning
+    {
+        public long ArtId { get; set; }
+        public string ArtNavn { get; set; } = string.Empty;
+        public DateTime AnkomstDato { get; set; }
+        public DateTime? SetIaarDato { get; set; }
+    }
