5bd271a9a2b53e2fb2211483d957f8671bc6e036
Thomas Elbek <vadholm@hotmail.com>
2026-05-14 11:36:16 +0200

    Update padding in App.vue template for improved layout

--- BirdWare.Frontend.TS/src/App.vue
+++ BirdWare.Frontend.TS/src/App.vue
@@ -4,7 +4,7 @@ import navigation from '@/components/main/navigation/navigation.vue';
 </script>
 
 <template>
-  <div class="max-w-full-xl mx-auto p-3">
+  <div class="max-w-full-xl mx-auto p-4 lg:p-0 lg:pt-3 lg:w-9/10">
     <div class="flex flex-col gap-y-1">
       <navigation />
       <RouterView />
