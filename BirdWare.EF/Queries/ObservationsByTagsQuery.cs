6709e0f481fb7dd63263b1d09b5f9617211054e2
Thomas Elbek <vadholm@hotmail.com>
2026-07-05 17:36:29 +0200

    Refactor GenerateResultSet LINQ and result mapping
    Refactored GenerateResultSet to project joined entities into an anonymous type, then map to VObs using AsEnumerable and Select. Updated the return to use a C# collection expression for improved clarity and maintainability.

--- BirdWare.EF/Queries/ObservationsByTagsQuery.cs
+++ BirdWare.EF/Queries/ObservationsByTagsQuery.cs
@@ -28,19 +28,20 @@ namespace BirdWare.EF.Queries
 
         private List<VObs> GenerateResultSet(IQueryable<Observation> observations)
         {
-            var result = (from obs in observations
-                          join tur in birdWareContext.Fugletur.AsNoTracking() on obs.FugleturId equals tur.Id
-                          join lok in birdWareContext.Lokalitet.AsNoTracking() on tur.LokalitetId equals lok.Id
-                          join kgrp in birdWareContext.Kommune.AsNoTracking() on lok.KommuneId equals kgrp.Id into kgroup
-                          from kom in kgroup.DefaultIfEmpty()
-                          join reg in birdWareContext.Region.AsNoTracking() on lok.RegionId equals reg.Id
-                          join art in birdWareContext.Art.AsNoTracking() on obs.ArtId equals art.Id
-                          join grp in birdWareContext.Gruppe.AsNoTracking() on art.GruppeId equals grp.Id
-                          join fam in birdWareContext.Familie.AsNoTracking() on grp.FamilieId equals fam.Id
-                          orderby tur.Dato descending
-                          select VObs.MapToVObs(obs, tur, lok, kom, reg, art, grp, fam)).Take(200).ToList();
+            var query = from obs in observations
+                join tur in birdWareContext.Fugletur.AsNoTracking() on obs.FugleturId equals tur.Id
+                join lok in birdWareContext.Lokalitet.AsNoTracking() on tur.LokalitetId equals lok.Id
+                join kgrp in birdWareContext.Kommune.AsNoTracking() on lok.KommuneId equals kgrp.Id into kgroup
+                from kom in kgroup.DefaultIfEmpty()
+                join reg in birdWareContext.Region.AsNoTracking() on lok.RegionId equals reg.Id
+                join art in birdWareContext.Art.AsNoTracking() on obs.ArtId equals art.Id
+                join grp in birdWareContext.Gruppe.AsNoTracking() on art.GruppeId equals grp.Id
+                join fam in birdWareContext.Familie.AsNoTracking() on grp.FamilieId equals fam.Id
+                orderby tur.Dato descending
+                select new { obs, tur, lok, kom, reg, art, grp, fam };
 
-            return result;
+                return [.. query.Take(200).AsEnumerable()
+                                .Select(x => VObs.MapToVObs(x.obs, x.tur, x.lok, x.kom, x.reg, x.art, x.grp, x.fam))];
         }        
     }
 }
