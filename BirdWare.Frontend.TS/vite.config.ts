37b1609bae2a0d7ca42a56172c74292d7a2fbf51
Thomas Elbek <vadholm@hotmail.com>
2026-09-18 16:55:42 +0200

    Added vite-plugin-pwa :-)

--- BirdWare.Frontend.TS/vite.config.ts
+++ BirdWare.Frontend.TS/vite.config.ts
@@ -1,5 +1,6 @@
 import { fileURLToPath, URL } from 'node:url'
 
+import { VitePWA } from 'vite-plugin-pwa'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import tailwindcss from '@tailwindcss/vite'
@@ -7,7 +8,50 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
-    vue(),
+    vue(), 
+    VitePWA({
+      strategies: 'generateSW',
+      registerType: 'autoUpdate',
+      includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'masked-icon.svg'],
+      workbox: {
+        runtimeCaching: [{
+          urlPattern: /^https:\/\/birdware\.dk\/api\//,
+          handler: "StaleWhileRevalidate",
+          method: 'GET',
+          options: {
+            cacheName: "api-cache",
+            expiration: {
+              maxEntries: 30,
+              maxAgeSeconds: 60 * 60 * 24
+            },
+            cacheableResponse: {
+              statuses: [0, 200]
+            }
+          }
+        }]
+      },
+      devOptions: {
+        enabled: true
+      },
+      manifest: {
+        name: 'Vue PWA Tutorial',
+        short_name: 'VuePWA',
+        description: 'A tutorial on building a resilient PWA with Vue and Vite',
+        theme_color: '#ffffff',
+        icons: [
+          {
+            src: 'pwa-192x192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: 'pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png'
+          }
+        ]
+      }
+    }),
     tailwindcss(),
   ],
   server: {
