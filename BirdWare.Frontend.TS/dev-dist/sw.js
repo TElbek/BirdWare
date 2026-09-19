24060471abbb33868fae03dbaf66a01cabafc8db
Thomas Elbek <vadholm@hotmail.com>
2026-09-19 07:12:08 +0200

    cvcxvc

--- BirdWare.Frontend.TS/dev-dist/sw.js
+++ BirdWare.Frontend.TS/dev-dist/sw.js
@@ -81,7 +81,7 @@ define(['./workbox-16ad23c7'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.1pnjsvoe9ng"
+    "revision": "0.4k45e5kh17o"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
