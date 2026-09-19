d9a0d43461ead22f5dcd0483ad2d39f1a63e655c
Thomas Elbek <vadholm@hotmail.com>
2026-09-19 07:56:21 +0200

    #
    Refactor FugleturAnalyseHandler dependencies & guards
    
    - Removed BirdWareContext from FugleturAnalyseHandler constructor; now only requires IFugleturAnalyseQuery.
    - Added guard conditions to Analyser switch cases using 'when' to ensure analyses only run with valid RegionId/KommuneId.
    - Improved switch case formatting for readability.

--- BirdWare/Business/FugleturAnalyseHandler.cs
+++ BirdWare/Business/FugleturAnalyseHandler.cs
@@ -1,11 +1,10 @@
 ﻿using BirdWare.Domain.Models;
-using BirdWare.EF;
 using BirdWare.EF.Interfaces;
 using BirdWare.Interfaces;
 
 namespace BirdWare.Business
 {
-    public class FugleturAnalyseHandler(BirdWareContext birdWareContext, IFugleturAnalyseQuery analyseQuery) : IFugleturAnalyseHandler
+    public class FugleturAnalyseHandler(IFugleturAnalyseQuery analyseQuery) : IFugleturAnalyseHandler
     {
         public IEnumerable<TripAnalysisResult> Analyser(long fugleturId, AnalyseTyper analyseType)
         {
@@ -14,13 +13,27 @@ namespace BirdWare.Business
 
             return analyseType switch
             {
-                AnalyseTyper.FoersteObsIDatabasen => [.. analyseQuery.FoersteObsIDatabasen(vTur, artsListe)],
-                AnalyseTyper.FoersteObsIDK => [.. analyseQuery.FoersteObsIDK(vTur, artsListe)],
-                AnalyseTyper.FoersteObsIRegion => [.. analyseQuery.FoersteObsIRegion(vTur, artsListe)],
-                AnalyseTyper.FoersteObsForKommune => [.. analyseQuery.FoersteObsForKommune(vTur, artsListe)],
-                AnalyseTyper.FoersteObsForLokalitet => [.. analyseQuery.FoersteObsForLokalitet(vTur, artsListe)],
-                AnalyseTyper.FoersteObsIAar => [.. analyseQuery.FoersteObsIAar(vTur, artsListe)],
-                AnalyseTyper.FoersteObsIMaaned => [.. analyseQuery.FoersteObsIMaaned(vTur, artsListe)],
+                AnalyseTyper.FoersteObsIDatabasen => 
+                    [.. analyseQuery.FoersteObsIDatabasen(vTur, artsListe)],
+
+                AnalyseTyper.FoersteObsIDK when vTur.RegionId > 0 => 
+                    [.. analyseQuery.FoersteObsIDK(vTur, artsListe)],
+
+                AnalyseTyper.FoersteObsIRegion => 
+                    [.. analyseQuery.FoersteObsIRegion(vTur, artsListe)],
+
+                AnalyseTyper.FoersteObsForKommune when vTur.RegionId > 0 && vTur.KommuneId > 0 => 
+                    [.. analyseQuery.FoersteObsForKommune(vTur, artsListe)],
+
+                AnalyseTyper.FoersteObsForLokalitet => 
+                    [.. analyseQuery.FoersteObsForLokalitet(vTur, artsListe)],
+
+                AnalyseTyper.FoersteObsIAar when vTur.RegionId > 0 => 
+                    [.. analyseQuery.FoersteObsIAar(vTur, artsListe)],
+
+                AnalyseTyper.FoersteObsIMaaned when vTur.RegionId > 0 => 
+                    [.. analyseQuery.FoersteObsIMaaned(vTur, artsListe)],
+
                 _ => throw new NotImplementedException()
             };
         }
